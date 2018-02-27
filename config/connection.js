d216947b08d8aa9e660125b39a294d9011aec4a7
Lysnandie <30992505+Lysnandie@users.noreply.github.com>
2018-02-26 23:35:03 -0600

    added correct controllers.js

--- config/connection.js
+++ config/connection.js
@@ -24,34 +24,3 @@ connection.connect(function(err) {
 
 // then export this connection for the ORM to use.
 module.exports = connection;
-
-// var mysql = require("mysql");
-// var connection;
-//
-// // var connection = mysql.createConnection({
-// //
-// //   host: "localhost",
-// //   user: "root",
-// //   password: "root",
-// //   database: "burgersapp_db"
-// // });
-//
-// if (process.env.JAWSDB_URL) {
-//   connection = mysl.createConnection(process.enc.JAWSDB_URL);
-// } else {
-//   host: "localhost",
-//   user: "root",
-//   password: "root",
-//   database: "burgersapp_db"
-// });
-//
-// connection.connect(function(err){
-//   if(err) {
-//   console.log("error connecting: " + err.stack);
-//   return;
-// }
-// console.log("connected as id: " + connection.threadId);
-// });
-//
-// //export connection for our ORM to use
-// module.exports = connection;
