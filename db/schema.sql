f0137510586e972b9c67dd4a12599c94c7e3a6c0
Lysnandie <30992505+Lysnandie@users.noreply.github.com>
2018-02-27 16:31:13 -0600

    retry jawsDB

--- db/schema.sql
+++ db/schema.sql
@@ -5,9 +5,9 @@ USE burgersapp_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
+	id int NOT AUTO_INCREMENT NOT NULL,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
-	date timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
